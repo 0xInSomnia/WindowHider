ffe82a5ce778eef237fbc40f19ebdfb583e0a7bc
griffinb <griffinb@BRAYDON-LAPTOP>
2021-09-17 09:25:48 +1000

    Add project files.

---
+++ WindowHider.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+
+}
